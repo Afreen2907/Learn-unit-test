f31d3759c2c73319e30dad9c96acc9a7c941a523
afreen2907 <afreen_fajila@outlook.com>
2022-03-23 22:20:28 +0800

    learned some testing from odin's

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(val) {
+    var split = val.split('')
+    var reverse = split.reverse()
+    var join = reverse.join('')
 
-};
+    return join
+}
 
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString
